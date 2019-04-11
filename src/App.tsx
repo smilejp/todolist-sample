4d9f0ff99b69b864855bb34008d90df09126f2d7
smilejp <smile.wjp@gmail.com>
2019-04-11 13:45:36 +0900

    feat: react hook을 사용하도록 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent, useState, useCallback } from 'react';
 import TodoListTemplate from './components/TodoListTemplate';
 import Form from './components/Form';
 import TodoItemList from './components/TodoItemList';
@@ -7,17 +7,16 @@ interface IProps {
   //
 }
 
-const App: FunctionComponent<IProps> = () => {
+function useCreate() {
   const [input, setInput] = useState('');
   const [todos, setTodos] = useState([
     { id: 0, text: 'Test1', checked: false },
     { id: 1, text: 'Test2', checked: true },
     { id: 2, text: 'Test3', checked: false },
   ]);
-
   const [id, setId] = useState(3);
 
-  function handleCreate() {
+  const handleCreate = useCallback(() => {
     if (input.length === 0) return;
 
     setInput('');
@@ -29,38 +28,114 @@ const App: FunctionComponent<IProps> = () => {
       })
     );
     setId(id + 1);
-  }
+  }, [input]);
 
-  function handleKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
-    if (e.key === 'Enter') {
-      handleCreate();
-    }
-  }
+  const handleKeyPress = useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === 'Enter') {
+        handleCreate();
+      }
+    },
+    [input]
+  );
 
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setInput(e.target.value);
-  }
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setInput(e.target.value);
+    },
+    [input]
+  );
 
-  function handleToggle(id?: number) {
-    const index = todos.findIndex(todo => todo.id === id);
-    const selected = todos[index];
-    const nextTodos = [...todos];
-    nextTodos[index] = {
-      ...selected,
-      checked: !selected.checked,
-    };
+  const handleToggle = useCallback(
+    (id?: number) => {
+      const index = todos.findIndex(todo => todo.id === id);
+      const selected = todos[index];
+      const nextTodos = [...todos];
+      nextTodos[index] = {
+        ...selected,
+        checked: !selected.checked,
+      };
 
-    setTodos(nextTodos);
-  }
+      setTodos(nextTodos);
+    },
+    [id, todos]
+  );
 
-  function handleRemove(id?: number) {
-    const index = todos.findIndex(todo => todo.id === id);
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
-  }
+  const handleRemove = useCallback(
+    (id?: number) => {
+      const index = todos.findIndex(todo => todo.id === id);
+      const newTodos = [...todos];
+      newTodos.splice(index, 1);
+      setTodos(newTodos);
+    },
+    [id, todos]
+  );
 
-  console.log('render App');
+  return { handleCreate, handleKeyPress, handleChange, handleToggle, handleRemove, todos, input };
+}
+
+const App: FunctionComponent<IProps> = () => {
+  // const [input, setInput] = useState('');
+  // const [todos, setTodos] = useState([
+  //   { id: 0, text: 'Test1', checked: false },
+  //   { id: 1, text: 'Test2', checked: true },
+  //   { id: 2, text: 'Test3', checked: false },
+  // ]);
+
+  // const [id, setId] = useState(3);
+
+  // const handleCreate = useCallback(() => {
+  //   if (input.length === 0) return;
+
+  //   setInput('');
+  //   setTodos(
+  //     todos.concat({
+  //       id: id + 1,
+  //       text: input,
+  //       checked: false,
+  //     })
+  //   );
+  //   setId(id + 1);
+  // }, [input]);
+
+  // function handleKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
+  //   if (e.key === 'Enter') {
+  //     handleCreate();
+  //   }
+  // }
+
+  // function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+  //   setInput(e.target.value);
+  // }
+
+  // function handleToggle(id?: number) {
+  //   const index = todos.findIndex(todo => todo.id === id);
+  //   const selected = todos[index];
+  //   const nextTodos = [...todos];
+  //   nextTodos[index] = {
+  //     ...selected,
+  //     checked: !selected.checked,
+  //   };
+
+  //   setTodos(nextTodos);
+  // }
+
+  // function handleRemove(id?: number) {
+  //   const index = todos.findIndex(todo => todo.id === id);
+  //   const newTodos = [...todos];
+  //   newTodos.splice(index, 1);
+  //   setTodos(newTodos);
+  // }
+  const {
+    input,
+    handleKeyPress,
+    handleChange,
+    handleCreate,
+    todos,
+    handleRemove,
+    handleToggle,
+  } = useCreate();
+  console.log(`render App timestamp:${new Date()}`);
   return (
     <TodoListTemplate
       form={
